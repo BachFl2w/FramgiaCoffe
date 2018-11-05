618035aa91dc18824be53eeb090f5223c55eda4b
bach <bachfl2w>
2018-11-05 10:45:48 +0700

    update model

--- app/OrderDetailTopings.php
+++ app/OrderDetailTopings.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetailTopings extends Model
 {
-    //
+    protected $table = 'order_detail_toping';
+
+    public function orderDetails()
+    {
+        return $this->belongsTo(OrderDetails::class, 'order_detail_id', 'id');
+    }
+
+    public function topings()
+    {
+        return $this->belongsTo(Toppings::class, 'topping_id', 'id');
+    }
 }
--- app/OrderDetails.php
+++ app/OrderDetails.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetails extends Model
 {
-    //
+    protected $table = 'order_details';
+
+    public function products()
+    {
+        return $this->belongsTo('App\Products', 'product_id', 'id');
+    }
+
+    public function orders()
+    {
+        return $this->belongsTo(Orders::class, 'order_id', 'id');
+    }
+
+    public function orderDetailTopings()
+    {
+        return $this->hasMany(OrderDetailTopings::class, 'order_detail_id', 'id');
+    }
+
+    public function sizes()
+    {
+        return $this->belongsTo(Sizes::class, 'size_id', 'id');
+    }
 }
