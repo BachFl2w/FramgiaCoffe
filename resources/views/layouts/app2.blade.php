f406124b801f57c756156e7e2a3a69c033eb89c2
BachNguyen <bachfl2w@gmail.com>
2018-11-06 08:07:56 +0700

    nothing change

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -7,7 +7,7 @@
     @routes()
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>SYSTEM MANAGER MARK</title>
+    <title>Amin</title>
     <meta name="description" content="Sufee Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -30,7 +30,7 @@
 
     <script src="{{ asset('js/jquery.dataTables.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/dataTables.bootstrap4.js') }}" defer></script>
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
     <script src="{{ asset('js/plugins.js') }}"></script>
    {{--  <script src="{{ asset('js/main.js') }}"></script> --}}
