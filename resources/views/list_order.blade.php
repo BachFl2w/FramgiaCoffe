d9b5f946ef6bf7b879f731385ade35e86d2e6df9
chinn2206 <nnchi1998@gmail.com>
2018-11-26 11:31:27 +0700

    update code

--- resources/views/list_order.blade.php
+++ resources/views/list_order.blade.php
@@ -8,30 +8,9 @@
 @section('content')
     <div class="container margin_60_35">
         <div class="row">
-            <div class="col-md-3">
+            
 
-                <div class="box_style_2 hidden-xs info">
-                    <h4 class="nomargin_top">Delivery time <i class="icon_clock_alt pull-right"></i></h4>
-                    <p>
-                        Delivery only in 30'
-                    </p>
-                    <hr>
-                    <h4>Secure payment <i class="icon_creditcard pull-right"></i></h4>
-                    <p>
-                        Payment method is diverse
-                    </p>
-                </div><!-- End box_style_1 -->
-
-                <div class="box_style_2 hidden-xs" id="help">
-                    <i class="icon_lifesaver"></i>
-                    <h4>{{ __('message.need_help') }}</h4>
-                    <a href="tel://004542344599" class="phone">{{ config('asset.constaint.phone') }}</a>
-                    <small>{{ __('message.monday') }} {{ __('message.to') }} {{ __('message.friday') }} {{ config('asset.constaint.time_help') }}</small>
-                </div>
-
-            </div><!-- End col-md-3 -->
-
-            <div class="col-md-9">
+            <div class="col-md-12">
                 <div class="box_style_2" id="order_process">
                     <h2 class="inner">Your Orders</h2>
                     <table class="table table-bordered" id="order_list">
