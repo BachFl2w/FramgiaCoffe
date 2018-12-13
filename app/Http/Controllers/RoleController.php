f3576b2c698a8c6ec921a65311907dee7a917c6a
BachNguyen <bachfl2w@gmail.com>
2018-12-13 08:52:05 +0700

    update real time

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -19,16 +19,6 @@ class RoleController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -41,28 +31,6 @@ class RoleController extends Controller
         $role->name = $request->name;
 
         $role->save();
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
